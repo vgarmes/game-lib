212a858a079234907c958f90627108140cceb2a5
Victor Garcia Mestre <vgmestre@gmail.com>
2022-08-09 00:38:49 +0200

    seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,40 @@
 import { prisma } from '../src/server/prisma';
 import games from '../data/games.json';
 import platforms from '../data/platforms.json';
+import blobs from '../data/active_storage_blobs.json';
+import attachments from '../data/active_storage_attachments.json';
+import { Cover, Game } from '@prisma/client';
 
 interface GameRaw {}
 
 const seedGames = async () => {
   const cleanData = games.map(
     ({
-      genre_id,
-      created_at,
-      updated_at,
+      id,
+      title,
+      in_collection,
+      completed,
+      edition,
       release_date,
-      buy_date,
       completed_date,
-      ...keepAttrs
+      buy_date,
+      buy_price,
+      rating,
+      comment,
+      platform_id,
     }) => ({
-      ...keepAttrs,
-      release_date: release_date && new Date(release_date),
-      buy_date: buy_date && new Date(buy_date),
-      completed_date: completed_date && new Date(completed_date),
+      id,
+      title,
+      inCollection: in_collection,
+      completed,
+      edition,
+      releaseDate: release_date && new Date(release_date),
+      completedDate: completed_date && new Date(completed_date),
+      buyDate: buy_date && new Date(buy_date),
+      buyPrice: buy_price,
+      rating,
+      comment,
+      platformId: platform_id,
     })
   );
   return prisma.game.createMany({
@@ -34,9 +50,32 @@ const seedPlatforms = async () => {
   });
 };
 
+const seedImages = async () => {
+  const covers: Omit<Cover, 'createdAt' | 'updatedAt' | 'metadata'>[] = [];
+  const data = attachments.forEach((attachment) => {
+    const blob = blobs.find((blob) => attachment.blob_id === blob.id);
+    if (!blob) return;
+
+    const { key, filename, content_type, metadata, byte_size, checksum } = blob;
+    covers.push({
+      id: attachment.id,
+      key,
+      filename,
+      contentType: content_type,
+      byteSize: byte_size,
+      checksum,
+      gameId: attachment.record_id,
+    });
+  });
+  return prisma.cover.createMany({
+    data: covers,
+  });
+};
+
 const main = async () => {
-  seedPlatforms();
+  //seedPlatforms();
   //seedGames();
+  seedImages();
 };
 
 main()
