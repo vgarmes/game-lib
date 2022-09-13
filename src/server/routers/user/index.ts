cd987b9a91fb9d792fd8f555fe8c39501dacd227
Victor Garcia Mestre <vgmestre@gmail.com>
2022-09-13 21:32:24 +0200

    remove redundant validation

--- src/server/routers/user/index.ts
+++ src/server/routers/user/index.ts
@@ -6,12 +6,6 @@ import { signupSchema } from './schema';
 export const userRouter = createRouter().mutation('signup', {
   input: signupSchema,
   async resolve({ input, ctx }) {
-    if (input.password !== input.passwordConfirm) {
-      throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'Password and password confirmation do not match',
-      });
-    }
     const passwordHash = await hashPassword(input.password);
     const user = await ctx.prisma.user.create({
       data: { username: input.username, email: input.email, passwordHash },
