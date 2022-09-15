9a1ddb82326b87ac481184987046952a6e689eca
Victor Garcia Mestre <vgmestre@gmail.com>
2022-09-15 11:33:21 +0200

    form components

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,24 +1,48 @@
-import { InputHTMLAttributes } from 'react';
+import { HTMLAttributes, InputHTMLAttributes } from 'react';
 import classNames from 'classnames';
+import React from 'react';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string;
 }
 
-const Input: React.FC<Props> = ({ label, error, ...rest }) => {
-  return (
-    <div>
-      <label>
-        {label}
-        <input
-          {...rest}
-          className={classNames({ 'border-red-700': !!error })}
-        />
-      </label>
-      {error && <p className="text-red-700">{error}</p>}
-    </div>
-  );
-};
+const Input = React.forwardRef<HTMLInputElement, Props>(
+  ({ label, error, className, ...rest }, ref) => {
+    const hasError = !!error;
+    return (
+      <div className={className}>
+        <label>
+          <span
+            className={classNames(
+              'mb-2 text-sm font-medium text-gray-900 dark:text-gray-300',
+              { 'text-red-700 dark:text-red-500': hasError }
+            )}
+          >
+            {label}
+          </span>
+          <input
+            ref={ref}
+            className={classNames(
+              'block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500',
+              {
+                'border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500 dark:border-red-500 dark:bg-gray-700 dark:text-red-500 dark:placeholder-red-500':
+                  hasError,
+              }
+            )}
+            {...rest}
+          />
+        </label>
+        {error && (
+          <p className="mt-2 text-sm font-medium text-red-600 dark:text-red-500">
+            {error}
+          </p>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
 
 export default Input;
---
+++ src/components/common/Toggle.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { InputHTMLAttributes } from 'react';
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  labelPosition?: 'left' | 'right';
+}
+const Toggle = React.forwardRef<HTMLInputElement, Props>(
+  ({ label, labelPosition = 'left', className, ...rest }, ref) => {
+    const labelElement = (
+      <span className="text-sm font-medium text-gray-900 dark:text-gray-300">
+        {label}
+      </span>
+    );
+    return (
+      <div className={className}>
+        <label className="flex cursor-pointer items-center gap-3">
+          {labelPosition === 'left' && labelElement}
+          <input
+            ref={ref}
+            type="checkbox"
+            value=""
+            id="checked-toggle"
+            className="peer sr-only"
+            {...rest}
+          />
+          <div className="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-0.5 after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+          {labelPosition === 'right' && labelElement}
+        </label>
+      </div>
+    );
+  }
+);
+
+Toggle.displayName = 'Toggle';
+
+export default Toggle;
