b96e08f90723d9f37b2c6a6cbaca2de47f66e731
Victor Garcia Mestre <vgmestre@gmail.com>
2022-10-01 23:07:39 +0200

    delete cover route

--- src/server/routers/cover/index.ts
+++ src/server/routers/cover/index.ts
@@ -1,23 +1,36 @@
 import { z } from 'zod';
 import { createProtectedRouter, createRouter } from '../../createRouter';
 
-const protectedCoverRouter = createProtectedRouter('ADMIN').mutation('create', {
-  input: z.object({
-    publicId: z.string(),
-    secureUrl: z.string(),
-    filename: z.string(),
-    format: z.string(),
-    byteSize: z.number(),
-    width: z.number(),
-    height: z.number(),
-    checksum: z.string(),
-    gameId: z.number().optional(),
-  }),
-  async resolve({ input, ctx }) {
-    return ctx.prisma.cover.create({
-      data: input,
-    });
-  },
-});
+const protectedCoverRouter = createProtectedRouter('ADMIN')
+  .mutation('create', {
+    input: z.object({
+      publicId: z.string(),
+      secureUrl: z.string(),
+      filename: z.string(),
+      format: z.string(),
+      byteSize: z.number(),
+      width: z.number(),
+      height: z.number(),
+      checksum: z.string(),
+      gameId: z.number().optional(),
+    }),
+    async resolve({ input, ctx }) {
+      return ctx.prisma.cover.create({
+        data: input,
+      });
+    },
+  })
+  .mutation('delete', {
+    input: z.object({
+      id: z.number(),
+    }),
+    async resolve({ input, ctx }) {
+      return ctx.prisma.cover.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    },
+  });
 
 export const coverRouter = createRouter().merge(protectedCoverRouter);
--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -15,3 +15,11 @@ export async function uploadImage(
   );
   return response.json();
 }
+
+export async function deleteImage(publicId: string) {
+  const response = await fetch(
+    `https://${process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_SECRET}@api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/resources/image/upload?public_ids[]=${publicId}`,
+    { method: 'DELETE' }
+  );
+  return response.json();
+}
