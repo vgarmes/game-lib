5ab3386f918e89f19ab716bea1c3939cb5d51394
Victor Garcia Mestre <vgmestre@gmail.com>
2022-10-22 23:25:53 +0200

    link game details page

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { inferQueryOutput } from '../utils/trpc';
+import Link from 'next/link';
 
 type Game = {
   id: number;
@@ -14,24 +14,28 @@ const GameList = ({ games }: props) => {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,_minmax(10rem,_1fr))] justify-items-center">
       {games.map((game) => (
-        <div key={game.id} className="flex w-40 flex-col items-center">
-          {game.coverUrl && (
-            <div className="relative h-28 w-28">
-              <Image
-                alt={`${game.title} cover`}
-                src={game.coverUrl}
-                layout="fill"
-                objectFit="contain"
-                objectPosition={'50% 50%'}
-              />
+        <Link key={game.id} href={`/game/${game.id}`} passHref>
+          <a>
+            <div className="flex w-40 flex-col items-center">
+              {game.coverUrl && (
+                <div className="relative h-28 w-28">
+                  <Image
+                    alt={`${game.title} cover`}
+                    src={game.coverUrl}
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition={'50% 50%'}
+                  />
+                </div>
+              )}
+              <div className="p-3">
+                <h1 className="text-center text-sm font-bold tracking-tight text-white">
+                  {game.title}
+                </h1>
+              </div>
             </div>
-          )}
-          <div className="p-3">
-            <h1 className="text-center text-sm font-bold tracking-tight text-white">
-              {game.title}
-            </h1>
-          </div>
-        </div>
+          </a>
+        </Link>
       ))}
     </div>
   );
