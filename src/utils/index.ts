180eb074b71f7feb754706d8957625ea8ca1a64f
Victor Garcia Mestre <vgmestre@gmail.com>
2022-12-18 15:26:49 +0100

    fix: improve types

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,11 +2,14 @@ export function toISODateString(date: Date) {
   return date.toISOString().split('T')[0];
 }
 
-export function groupBy<T>(objectArray: T[], property: keyof T & string) {
+export function groupBy<T extends Record<K, any>, K extends string>(
+  objectArray: T[],
+  property: K
+) {
   return objectArray.reduce((acc, obj) => {
-    const key = obj[property] as unknown as string;
+    const key = obj[property];
     const curGroup = acc[key] ?? [];
 
     return { ...acc, [key]: [...curGroup, obj] };
-  }, {} as { [k: string]: T[] });
+  }, {} as Record<T[K], T[]>);
 }
