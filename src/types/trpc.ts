a4143d1e5527040d354656cdd75c0c01a75b4266
Victor Garcia Mestre <vgmestre@gmail.com>
2024-12-23 22:57:10 +0100

    refactor: remove trpc v10 unused types

--- src/types/trpc.ts
+++ src/types/trpc.ts
@@ -1,10 +1,5 @@
 import { AppRouter } from '@/server/routers/_app';
-import type {
-  inferProcedureInput,
-  inferProcedureOutput,
-  inferRouterInputs,
-  inferRouterOutputs,
-} from '@trpc/server';
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
 import { NextPageContext } from 'next';
 
 export interface SSRContext extends NextPageContext {
@@ -16,11 +11,3 @@ export type RouterOutput = inferRouterOutputs<AppRouter>;
 
 export type Games = RouterOutput['game']['search'];
 export type Game = Games[number];
-
-export type inferMutationOutput<
-  TRouteKey extends keyof AppRouter['_def']['mutations']
-> = inferProcedureOutput<AppRouter['_def']['mutations'][TRouteKey]>;
-
-export type inferMutationInput<
-  TRouteKey extends keyof AppRouter['_def']['mutations']
-> = inferProcedureInput<AppRouter['_def']['mutations'][TRouteKey]>;
