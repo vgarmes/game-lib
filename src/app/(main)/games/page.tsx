1ca1b8b95e0c9c56ddceea8176886cc96532dbad
Victor Garcia Mestre <vgmestre@gmail.com>
2026-04-05 00:10:29 +0200

    feat: wip

--- src/app/(main)/games/page.tsx
+++ src/app/(main)/games/page.tsx
@@ -14,6 +14,7 @@ import {
   formatDistanceToNow,
   formatDistanceToNowStrict,
 } from "date-fns";
+import { Stars } from "@/components/common";
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: "long",
@@ -42,66 +43,64 @@ export default function GamesPage() {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <div className="rounded-lg border">
+        <div className="flex flex-col gap-2 md:gap-0">
           {flatData.map((game) => (
             <div
               key={game.id}
-              className="flex items-start gap-4 p-4 not-last:border-b"
+              className="bg-card/50 flex flex-col gap-4 overflow-hidden rounded-lg border p-4 md:flex-row md:gap-8 md:rounded-none md:not-last:border-b-0 md:first:rounded-t-lg md:last:rounded-b-lg"
             >
-              <div className="relative size-12 overflow-hidden rounded">
-                <Image
-                  alt={game.title}
-                  src={game.cover?.secureUrl ?? "/placeholder.png"}
-                  fill
-                  className="size-4 rounded object-cover"
-                />
-              </div>
+              <div className="flex min-w-80 flex-1 items-center gap-4 overflow-hidden">
+                <div className="relative size-12 shrink-0 overflow-hidden rounded">
+                  <Image
+                    alt={game.title}
+                    src={game.cover?.secureUrl ?? "/placeholder.png"}
+                    fill
+                    className="size-4 rounded object-cover"
+                  />
+                </div>
 
-              <div className="flex max-w-80 flex-1 flex-col gap-1">
-                <div className="flex items-center gap-2">
-                  <div className="truncate text-sm font-medium">
-                    {game.title}
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center gap-2">
+                    <div className="truncate text-sm font-medium">
+                      {game.title}
+                    </div>
+                    {game.edition && (
+                      <Badge variant="secondary">{game.edition}</Badge>
+                    )}
                   </div>
-                  {game.edition && (
-                    <Badge variant="secondary">{game.edition}</Badge>
-                  )}
-                </div>
-                <div className="text-muted-foreground text-sm">
-                  {game.platform?.name}
+                  <div className="text-muted-foreground text-sm">
+                    {game.platform?.name}
+                  </div>
                 </div>
               </div>
 
-              <div className="flex flex-col gap-1">
+              <div className="flex max-w-40 flex-1 flex-col">
+                <Stars activeStar={game.rating ? game.rating - 1 : 0} />
+              </div>
+
+              <div className="flex max-w-30 flex-[1_1_auto] flex-col gap-2 overflow-hidden">
                 {game.completed ? (
-                  <div className="text-muted-foreground flex flex-col gap-1 text-sm">
+                  <div className="text-muted-foreground flex min-w-px flex-col gap-1 overflow-hidden text-sm">
                     <div className="-ml-[3px] flex items-center gap-1">
-                      <span className="flex size-4 items-center justify-center">
+                      <span className="flex size-4 shrink-0 items-center justify-center">
                         <span className="size-2 flex-none rounded-full bg-green-400" />
                       </span>
-                      Completed
-                      {game.completedDate && (
-                        <span>
-                          {" "}
-                          {formatDistanceToNowStrict(game.completedDate, {
-                            addSuffix: true,
-                          })}
-                        </span>
-                      )}
+                      <span className="truncate">Completed</span>
                     </div>
-
-                    {game.completedDate && (
-                      <div>
-                        <span>{dateFormatter.format(game.completedDate)}</span>
-                      </div>
-                    )}
                   </div>
                 ) : (
                   <div className="text-muted-foreground -ml-[3px] flex items-center gap-1 text-sm">
-                    <span className="flex size-4 items-center justify-center">
+                    <span className="flex size-4 shrink-0 items-center justify-center">
                       <span className="size-2 flex-none rounded-full bg-neutral-400" />
                     </span>
-                    Not Completed
+                    <span className="truncate">Backlog</span>
                   </div>
+                )}
+                {game.completed && game.completedDate && (
+                  <span className="text-muted-foreground truncate text-sm">
+                    {" "}
+                    {game.completedDate.toLocaleDateString()}
+                  </span>
                 )}
               </div>
             </div>
