53ad05eef0871efc06daaebcfd79c40ab27cb41d
Victor Garcia Mestre <vgmestre@gmail.com>
2026-04-05 23:31:05 +0200

    chore: wrap page in suspense

--- src/app/(main)/games/page.tsx
+++ src/app/(main)/games/page.tsx
@@ -10,12 +10,23 @@ import { Stars } from "@/components/common";
 import { Search } from "lucide-react";
 import { PlatformSelector } from "@/components/platform-selector";
 import { parseAsInteger, parseAsString, useQueryStates } from "nuqs";
+import { Suspense } from "react";
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: "long",
 });
 
 export default function GamesPage() {
+  return (
+    <PageLayout breadcrumbs={undefined}>
+      <Suspense>
+        <Content />
+      </Suspense>
+    </PageLayout>
+  );
+}
+
+function Content() {
   const [query, setQuery] = useQueryStates({
     searchText: parseAsString.withDefault(""),
     platformId: parseAsInteger,
@@ -33,107 +44,105 @@ export default function GamesPage() {
   const flatData = data?.pages.flatMap((page) => page.items) ?? [];
 
   return (
-    <PageLayout breadcrumbs={undefined}>
-      <div className="flex flex-col gap-4 px-4 pb-4 lg:px-6">
-        <div className="flex items-center gap-4">
-          <div className="relative flex-1">
-            <Input
-              className="h-10 pl-8"
-              placeholder="Search games..."
-              value={query.searchText}
-              onChange={(e) => setQuery({ searchText: e.target.value })}
-            />
-            <Search className="text-muted-foreground absolute top-1/2 left-2.5 size-4 -translate-y-1/2" />
-          </div>
-          <PlatformSelector
-            selectedId={query.platformId ?? undefined}
-            onSelect={(value) =>
-              setQuery({
-                platformId: query.platformId === value ? null : value,
-              })
-            }
-            className="h-10"
-            placeholder="All platforms"
+    <div className="flex flex-col gap-4 px-4 pb-4 lg:px-6">
+      <div className="flex items-center gap-4">
+        <div className="relative flex-1">
+          <Input
+            className="h-10 pl-8"
+            placeholder="Search games..."
+            value={query.searchText}
+            onChange={(e) => setQuery({ searchText: e.target.value })}
           />
+          <Search className="text-muted-foreground absolute top-1/2 left-2.5 size-4 -translate-y-1/2" />
         </div>
-        <div className="flex flex-col gap-2 md:gap-0">
-          {flatData.map((game) => (
-            <div
-              key={game.id}
-              className="bg-card/50 flex flex-col gap-4 overflow-hidden rounded-lg border p-4 md:grid md:grid-cols-[3fr_1fr_minmax(0,100px)] md:gap-8 md:rounded-none md:not-last:border-b-0 md:first:rounded-t-lg md:last:rounded-b-lg"
-            >
-              <div className="flex items-center gap-4 overflow-hidden">
-                <div className="relative size-12 shrink-0 overflow-hidden rounded">
-                  <Image
-                    alt={game.title}
-                    src={game.cover?.secureUrl ?? "/placeholder.png"}
-                    fill
-                    className="size-4 rounded object-cover"
-                  />
-                </div>
-
-                <div className="flex flex-col gap-2 overflow-hidden">
-                  <div className="flex min-w-0 items-center gap-2">
-                    <div className="truncate text-sm font-medium">
-                      {game.title}
-                    </div>
-                    {game.edition && (
-                      <Badge variant="secondary" className="shrink">
-                        <span className="truncate">{game.edition}</span>
-                      </Badge>
-                    )}
-                  </div>
-                  <div className="text-muted-foreground text-sm">
-                    {game.platform?.name}
-                  </div>
-                </div>
+        <PlatformSelector
+          selectedId={query.platformId ?? undefined}
+          onSelect={(value) =>
+            setQuery({
+              platformId: query.platformId === value ? null : value,
+            })
+          }
+          className="h-10"
+          placeholder="All platforms"
+        />
+      </div>
+      <div className="flex flex-col gap-2 md:gap-0">
+        {flatData.map((game) => (
+          <div
+            key={game.id}
+            className="bg-card/50 flex flex-col gap-4 overflow-hidden rounded-lg border p-4 md:grid md:grid-cols-[3fr_1fr_minmax(0,100px)] md:gap-8 md:rounded-none md:not-last:border-b-0 md:first:rounded-t-lg md:last:rounded-b-lg"
+          >
+            <div className="flex items-center gap-4 overflow-hidden">
+              <div className="relative size-12 shrink-0 overflow-hidden rounded">
+                <Image
+                  alt={game.title}
+                  src={game.cover?.secureUrl ?? "/placeholder.png"}
+                  fill
+                  className="size-4 rounded object-cover"
+                />
               </div>
 
-              <div className="flex w-full flex-col overflow-hidden md:items-end">
-                <Stars activeStar={game.rating ? game.rating - 1 : 0} />
-              </div>
-
-              <div className="flex w-full justify-between gap-2 overflow-hidden md:flex-col">
-                {game.completed ? (
-                  <div className="text-muted-foreground flex min-w-px flex-col gap-1 overflow-hidden text-sm">
-                    <div className="-ml-[3px] flex items-center gap-1">
-                      <span className="flex size-4 shrink-0 items-center justify-center">
-                        <span className="size-2 flex-none rounded-full bg-green-400" />
-                      </span>
-                      <span className="truncate">Completed</span>
-                    </div>
+              <div className="flex flex-col gap-2 overflow-hidden">
+                <div className="flex min-w-0 items-center gap-2">
+                  <div className="truncate text-sm font-medium">
+                    {game.title}
                   </div>
-                ) : (
-                  <div className="text-muted-foreground -ml-[3px] flex items-center gap-1 text-sm">
-                    <span className="flex size-4 shrink-0 items-center justify-center">
-                      <span className="size-2 flex-none rounded-full bg-neutral-400" />
-                    </span>
-                    <span className="truncate">Backlog</span>
-                  </div>
-                )}
-                {game.completed && game.completedDate && (
-                  <span className="text-muted-foreground truncate text-sm">
-                    {" "}
-                    {game.completedDate.toLocaleDateString()}
-                  </span>
-                )}
+                  {game.edition && (
+                    <Badge variant="secondary" className="shrink">
+                      <span className="truncate">{game.edition}</span>
+                    </Badge>
+                  )}
+                </div>
+                <div className="text-muted-foreground text-sm">
+                  {game.platform?.name}
+                </div>
               </div>
             </div>
-          ))}
-        </div>
-        {hasNextPage && (
-          <Button
-            variant="outline"
-            className="h-10"
-            onClick={() => {
-              fetchNextPage();
-            }}
-            disabled={isFetchingNextPage}
-          >
-            Load More
-          </Button>
-        )}
+
+            <div className="flex w-full flex-col overflow-hidden md:items-end">
+              <Stars activeStar={game.rating ? game.rating - 1 : 0} />
+            </div>
+
+            <div className="flex w-full justify-between gap-2 overflow-hidden md:flex-col">
+              {game.completed ? (
+                <div className="text-muted-foreground flex min-w-px flex-col gap-1 overflow-hidden text-sm">
+                  <div className="-ml-[3px] flex items-center gap-1">
+                    <span className="flex size-4 shrink-0 items-center justify-center">
+                      <span className="size-2 flex-none rounded-full bg-green-400" />
+                    </span>
+                    <span className="truncate">Completed</span>
+                  </div>
+                </div>
+              ) : (
+                <div className="text-muted-foreground -ml-[3px] flex items-center gap-1 text-sm">
+                  <span className="flex size-4 shrink-0 items-center justify-center">
+                    <span className="size-2 flex-none rounded-full bg-neutral-400" />
+                  </span>
+                  <span className="truncate">Backlog</span>
+                </div>
+              )}
+              {game.completed && game.completedDate && (
+                <span className="text-muted-foreground truncate text-sm">
+                  {" "}
+                  {game.completedDate.toLocaleDateString()}
+                </span>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
-    </PageLayout>
+      {hasNextPage && (
+        <Button
+          variant="outline"
+          className="h-10"
+          onClick={() => {
+            fetchNextPage();
+          }}
+          disabled={isFetchingNextPage}
+        >
+          Load More
+        </Button>
+      )}
+    </div>
   );
 }
